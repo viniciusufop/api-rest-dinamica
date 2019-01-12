c8ec9ce68f723bcdb2c81ddccc17d9bdfc416f2a
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-01-12 13:57:36 -0200

    criando todos os metodos rest do crud

--- src/main/java/br/com/vfs/apirestdinamica/controller/ApiController.java
+++ src/main/java/br/com/vfs/apirestdinamica/controller/ApiController.java
@@ -1,23 +1,43 @@
 package br.com.vfs.apirestdinamica.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api")
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
 public class ApiController {
 
     @GetMapping("/{model}")
     public ResponseEntity getAllElementsFromModel(@PathVariable(name = "model") final String model) {
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body(model);
     }
 
     @GetMapping("/{model}/{id}")
-     public ResponseEntity getElementByIdFromModel(@PathVariable(name = "model") final String model) {
+    public ResponseEntity getElementFromModelById(@PathVariable(name = "model") final String model,
+                                                  @PathVariable(name = "id") final Integer id) {
+        return ResponseEntity.ok().body(model + " " + id.toString());
+    }
 
-        return ResponseEntity.ok().build();
-     }
+    @PostMapping("/{model}")
+    public ResponseEntity createNewElementFromModel(@PathVariable(name = "model") final String model,
+                                                    @RequestBody final String structElement) throws URISyntaxException {
+        return ResponseEntity.created(new URI("12")).body(model + " " + structElement);
+    }
 
+    @PutMapping("/{model}/{id}")
+    public ResponseEntity alterElementFromModelByID(@PathVariable(name = "model") final String model,
+                                                    @PathVariable(name = "id") final Integer id,
+                                                    @RequestBody final String structElement) throws URISyntaxException {
+        return ResponseEntity.created(new URI("12")).body(model + " " + structElement);
+    }
+
+    @DeleteMapping("/{model}/{id}")
+    public ResponseEntity removeElementFromModelByID(@PathVariable(name = "model") final String model,
+                                                     @PathVariable(name = "id") final Integer id) {
+        return ResponseEntity.ok().body(model + " " + id.toString());
+    }
 }
